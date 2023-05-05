4250c7b7e45e0ed85bbba6b4dea962dda38483ee
SilveStredecendent <herbertdesousasantos31@gmail.com>
2023-05-05 15:13:18 +0000

    Salvar

--- app/src/main/java/startspring/WebApplication.java
+++ app/src/main/java/startspring/WebApplication.java
@@ -1,5 +1,12 @@
 package startspring;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class WebApplication {
+
+    public static void public static void main(String[] args) {
+        SpringBootApplication.run(WebApplication.class, args);
+    }
     
 }
